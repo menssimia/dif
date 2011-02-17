1d63f3e75f95c945f3c2a24914cbdc10e39ee815
Jan Adelsbach <jan.software@web.de>
2011-02-17 16:04:48 +0100

    Updated Date to 2011

--- include/dif.h
+++ include/dif.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, Jan Adelsbach and other authors and contributors
+ * Copyright (C) 2010, 2011 Jan Adelsbach and other authors and contributors
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
